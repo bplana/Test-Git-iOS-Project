9ee329ea5ddb3e18f12f24933984191cb50d2720
bernadette <bernadette@Temps-MacBook-Pro.local>
2015-02-24 17:26:34 -0800

    added a test class

---
+++ Git Test/BPTest.h
@@ -0,0 +1,13 @@
+//
+//  BPTest.h
+//  Git Test
+//
+//  Created by bernadette on 2/24/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BPTest : NSObject
+
+@end
---
+++ Git Test/BPTest.m
@@ -0,0 +1,13 @@
+//
+//  BPTest.m
+//  Git Test
+//
+//  Created by bernadette on 2/24/15.
+//
+//
+
+#import "BPTest.h"
+
+@implementation BPTest
+
+@end
